948bd2957b79f0e7524f1ab59ad286aecc9aa68c
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-19 15:32:59 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    $users = User::paginate(5);
+    return view('dashboard', compact('users'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';

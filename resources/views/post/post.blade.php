801e5ac679e5bcc11c2297a78b159df9a1065da7
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-21 14:13:18 +0000

    create posts table

---
+++ resources/views/post/post.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <!-- Create New Button -->
+                    <div class="mb-4">
+                        <button type="button" class="btn btn-primary">Create New Post</button>
+                    </div>
+
+                    <!-- Table -->
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Created At</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{ $post->id }}</td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{{ $post->category }}</td>
+                                    <td>{{ $post->created_at->format('Y-m-d') }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use App\DataTables\UsersDataTable;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use Spatie\Permission\Models\Permission;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
@@ -20,3 +24,33 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::get('create-role', function () {
+    // $role = Role::create(['name' => 'writer']);
+
+    // return $role;
+
+    // $permission = Permission::create(['name' => 'edit articles']);
+
+    // return $permission;
+
+    $user = auth()->user();
+    // $user->assignRole('writer');
+    // $user->load('roles');
+
+    // $user->givePermissionTo('edit articles');
+    // $user->load('permissions');
+
+    // $checkPermission = $user->can('edit articles');
+
+    if ($user->can('edit articles')) {
+        return 'user has permission';
+    } else {
+        return 'user does not have permission';
+    }
+});
+
+Route::get('posts', function () {
+    $posts = Post::all();
+    return view('post.post', compact('posts'));
+});
